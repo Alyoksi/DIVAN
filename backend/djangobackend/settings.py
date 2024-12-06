9bb5c5b5849dad12c1cf9ab3ef348257426da217
Ivan Severikov <severikov2004@gmail.com>
2024-12-06 05:26:51 +0300

    docker

--- backend/djangobackend/settings.py
+++ backend/djangobackend/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -77,10 +78,10 @@ WSGI_APPLICATION = 'djangobackend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': '',
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': 'localhost',  # или IP адрес сервера базы данных
+        'NAME': os.environ.get('POSTGRES_NAME'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': 'db',  # или IP адрес сервера базы данных
         'PORT': '5432',  # порт по умолчанию для PostgreSQL
     }
 }
