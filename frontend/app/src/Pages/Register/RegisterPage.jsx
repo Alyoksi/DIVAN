547e51b76fd435ef8933acffe0580246754771e5
tro11ing <karamberkogach@bk.ru>
2024-12-06 13:45:59 +0300

    Фронт странички регистрации чтоб был

--- frontend/app/src/Pages/Register/RegisterPage.jsx
+++ frontend/app/src/Pages/Register/RegisterPage.jsx
@@ -1,12 +1,46 @@
 import React from 'react';
+import styled from 'styled-components';
+import Header from '../../components/Headers/BasicHeader/BasicHeader';
+import './RegisterPage.scss'
 
-// RegisterPage.js
-function RegisterPage() {
+const Button = styled.button`
+  background-color: #9A9A9A;
+  color: #FFFFFF;
+  border-radius: 5px;
+  border: none;
+  padding: 30px 0px;
+  margin-top: 20px;
+  width: 250px;
+
+  font-size: 20px;
+
+  cursor: pointer;
+
+  &:hover {
+    background-color: #bfbdbd;
+  }
+`;
+
+const RegisterPage = () => {
     return (
-      <div>
-        <h1>Страница регистрации</h1>
-        {/* Форма регистрации */}
+      <div className='title'>
+        <Header />
+        <div className="container">
+          <p className='form_name'>Регистрация</p>
+          <div className="rectangle">
+            <p>Логин (Username в Telegram)</p>
+            <input type="text" placeholder="Username" id="username" required />
+            <p>Пароль</p>
+            <input type="password" placeholder="Password" id="password" required/>
+            <p>Подтверждение пароля</p>
+            <input type="password" placeholder="Password" id="confirm_password" required/>
+          </div>
+          <Button>
+            Создать аккаунт
+          </Button>
+        </div>
       </div>
     );
-  }
-  export default RegisterPage;
+};
+
+export default RegisterPage;
